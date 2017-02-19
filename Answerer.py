21462b830e6f964beb363282f87c2df1f5cb32dd
Faraz Fadavi <fadavi@stanford.edu>
2017-02-18 18:23:57 -0800

    formatted code

--- Answerer.py
+++ Answerer.py
@@ -8,62 +8,66 @@ import itertools as it
 import re
 import sys
 
+
 class Answerer:
+
     def answer(self, parsed_clues):
         """Answer each clue and return a list of answers."""
         answers = []
         wiki_filename = "data/wiki-text-ner.txt"
-        
+
         # TODO: Process the wiki file and fill in the answers list.
         # Add 'No answer.' as the answer when you don't find an answer you are confident of.
-        # We recommend using Hearst style patterns to find answers within the Wiki text.
+        # We recommend using Hearst style patterns to find answers within the
+        # Wiki text.
         for parsed_clue in parsed_clues:
             clue_type, clue_entity = parsed_clue.split(":")
             if clue_type == "born_in":
                 answers.append(self.answerBornIn(clue_entity))
             else:
-                match = self.searchForPatterns([r'(n) (\w{1,10}) Poe'], [0], wiki_filename)
+                match = self.searchForPatterns(
+                    [r'(n) (\w{1,10}) Poe'], [0], wiki_filename)
                 answers.append("What is 1492?")
         return answers
-    
+
     # TODO: you may want to declare functions like these to structure
     # your code, but feel free to remove this function if you don't need it.
     def answerBornIn(self, clue_entity):
         """Answers questions of the type born_in:[clue_entity]."""
         return "What is Stanford, California?"
 
-
     # TODO: This method is merely a piece of starter code that we think you will find useful. If you use it,
     # you may want to modify it to make it more useful. You may modify it however you like, and you may also
-    # change the return type or the argument list, since we won't call this function directly during grading.
+    # change the return type or the argument list, since we won't call this
+    # function directly during grading.
     def searchForPatterns(self, pattern_strings, pattern_positions, filename):
         """Searches a text file using several regular expressions at the same time, and returns one match. If more than
             one regular expression matches a piece of the file, the order of the regular expressions in the list
             is used to decide which match to return, so you should place more precise regular expressions earlier
             in the list. If more than one piece of the file matches the same regular expression, the last match
             in the file will be returned.
-            
+
             patternStrings should be a list of regular experessions, ordered from most precise to least precise.
-            
+
             patternPositions should be a list of integers of the same length as the list of regular expressions. and
             should indicate which group within each regular expression should be returned. Group 0 contains the entire
             match, group 1 contains the piece of the match inside first set of parantheses of the regular expression,
             group 2 contains the piece inside the second set of parentheses, etc.
-            
+
             For example, for the regular expression "He was (maybe|probably) born in (\\d\\d\\d\\d)" and a file with
             the line "He was maybe born in 1899, and died in 1867.", these will be the groups you can choose from:
             - Group 0: "He was maybe born in 1899"
             - Group 1: "maybe"
             - Group 2: "1899"
-            
+
             So if "1899" is the string that you want to include in your Jeopardy answer, you set the entry in patternPositions
             corresponding to this regular exrpession to 2.
-            
+
             filename should be the filename for the text file being searched."""
         patterns = []
         for pattern_string in pattern_strings:
             patterns.append(re.compile(pattern_string, re.IGNORECASE))
-        
+
         # Iterate over the lines of the file.
         lines = loadList(filename)
         matched_groups = {}
@@ -72,32 +76,34 @@ class Answerer:
                 m = p.search(line)
                 if m:
                     matched_groups[i] = m.group(pattern_positions[i])
-        
-        # Return the match corresponding to the first regular expression in the list.
+
+        # Return the match corresponding to the first regular expression in the
+        # list.
         if len(matched_groups) > 0:
             return matched_groups[min(matched_groups.keys())]
         else:
             return None
-    
-    #### You should not need to change anything after this point. ####
+
+#### You should not need to change anything after this point. ####
 
     def evaluate(self, guessed_answers, guessed_answers_from_parses, gold_answers):
         """Shows you how your model will score on the training/development data."""
         print "Guessed answers from clues:"
-        score1 = self.evaluateAnswerSet(guessed_answers, gold_answers);
+        score1 = self.evaluateAnswerSet(guessed_answers, gold_answers)
         print "Guessed answers from gold parses:"
-        score2 = self.evaluateAnswerSet(guessed_answers_from_parses, gold_answers);
+        score2 = self.evaluateAnswerSet(
+            guessed_answers_from_parses, gold_answers)
         print "Total score: {0}".format(score1 + score2)
-    
+
     def evaluateAnswerSet(self, guessed_answers, gold_answers):
         """Scores one set of answers."""
         correct = 0
         wrong = 0
         no_answers = 0
         score = 0
-        
+
         for guessed_answer, gold_answer_line in it.izip(guessed_answers, gold_answers):
-            example_wrong = True # guilty until proven innocent
+            example_wrong = True  # guilty until proven innocent
             if guessed_answer == "No answer.":
                 example_wrong = False
                 no_answers += 1
@@ -116,14 +122,16 @@ class Answerer:
         print "No Answers: {0}".format(no_answers)
         print "Wrong Answers: {0}".format(wrong)
         print "Score: {0}".format(score)
-        
+
         return score
+
 
 def loadList(file_name):
     """Loads text files as lists of lines. Used in evaluation."""
     with open(file_name) as f:
         l = [line.strip() for line in f]
     return l
+
 
 def main():
     """Tests the model on the command line. This won't be called in
@@ -136,17 +144,17 @@ def main():
     clues_val_file = "data/part2-clues-val.txt"
     parses_val_file = "data/part2-parses-val.txt"
     gold_val_file = "data/part2-gold-val.txt"
-    
+
     training_clues_file = "data/part1-clues.txt"
     training_parsed_clues_file = "data/part1-parsedclues.txt"
-    
+
     a = Answerer()
-    
+
     # Test on unparsed questions.
     clues = loadList(clues_file)
     training_clues = loadList(training_clues_file)
     training_parsed_clues = loadList(training_parsed_clues_file)
-    
+
     cp = ClueParser()
     cp.train(training_clues, training_parsed_clues)
     guessed_parses = cp.parseClues(clues)
@@ -154,7 +162,7 @@ def main():
     # Test on parsed questions.
     gold_parses = loadList(parses_file)
     guessed_answers_from_parses = a.answer(gold_parses)
-    
+
     # Evaluate both sets of answers.
     gold_answers = loadList(gold_file)
     a.evaluate(guessed_answers, guessed_answers_from_parses, gold_answers)
@@ -168,11 +176,11 @@ def main():
         # Test on parsed questions.
         gold_parses_val = loadList(parses_val_file)
         guessed_answers_val_from_parses = a.answer(gold_parses_val)
-        
+
         # Evaluate both sets of answers.
         gold_answers_val = loadList(gold_val_file)
-        a.evaluate(guessed_answers_val, guessed_answers_val_from_parses, gold_answers_val)
-
+        a.evaluate(guessed_answers_val,
+                   guessed_answers_val_from_parses, gold_answers_val)
 
 
 if __name__ == '__main__':
